93454be91ed6e891f7dfc215bb7837e1d2ba15d1
Jason Ouyang <jasonouyang8000@gmail.com>
2021-05-24 13:55:11 -0700

    react-modal

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -2,5 +2,5 @@ import socketio from "socket.io-client";
 
 import { createContext } from "react";
 
-export const socket = socketio.connect(process.env.REACT_APP_URL, { query: {usertoken: localStorage.getItem('usertoken')}});
+export const socket = socketio.connect(process.env.REACT_APP_URL);
 export const SocketContext = createContext();
